4bc7ee329d38b596dcb617d8459c8ad50157ebae
Minh Tran <minh.tv1411@gmail.com>
2023-03-07 22:46:16 +0700

    bai tap ngay 07-03

---
+++ Day-07.03/bai3-4.js
@@ -0,0 +1,6 @@
+inputRadius = prompt("enter the radius");
+let radius = parseInt(inputRadius);
+let acreage = radius * radius * 3.14;
+let perimeter = radius * 2 *3.13;
+document.write("acreage:" +acreage);
+document.write("perimeter:" +perimeter);
